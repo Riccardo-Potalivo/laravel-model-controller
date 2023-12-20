6754a4279f3b1421da093ff2cd07d2df15074a19
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-12-20 15:54:57 +0100

    route-and-controller-movies-set

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movie = Movie::all();
+        dd($movie);
+        return view("movies.index", compact("movie"));
+    }
+}
---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Movies')
+
+@section('content')
+
+    <section>
+        <h1>Movies</h1>
+    </section>
+
+@endsection
