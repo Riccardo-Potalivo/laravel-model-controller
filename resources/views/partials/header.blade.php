5438caa087141d423cb56ca524ab1d74c8af3fee
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-12-20 18:05:50 +0100

    navbar-set

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,24 @@
 <div>
-    header
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Model Controller</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link {{ Route::currentRouteName() == 'movies.index' ? 'active' : '' }}"
+                            href="{{ route('movies.index') }}">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ Route::currentRouteName() == 'books.index' ? 'active' : '' }}"
+                            href="{{ route('books.index') }}">Books</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
 </div>
