c984f9b92b52b1a5ab5e73c1517446bb1d2978a3
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-12-20 16:04:07 +0100

    home-controller-set

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MovieController;
 
 /*
@@ -14,8 +15,8 @@ use App\Http\Controllers\MovieController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
+
+// Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
